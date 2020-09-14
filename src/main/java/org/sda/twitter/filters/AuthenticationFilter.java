9a5a1f8c1408ff165e6d626c817941f9b6c476e6
Tomasz Krutowski <tkrutowski@gmail.com>
2020-09-14 19:47:52 +0200

    Added AuthenticationFilter

---
+++ src/main/java/org/sda/twitter/filters/AuthenticationFilter.java
@@ -0,0 +1,34 @@
+package org.sda.twitter.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthenticationFilter", servletNames = {}, urlPatterns = {"/profile.jsp"})
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpSession session = httpServletRequest.getSession(false);
+
+        if (session != null) {
+            chain.doFilter(request,response);
+        } else {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
