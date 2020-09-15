6c5596e96d0587de7566bdb20ae64717cddd5e86
Tomasz Krutowski <tkrutowski@gmail.com>
2020-09-15 22:49:33 +0200

    added a method which get all users from db without logged user

--- src/main/java/org/sda/twitter/database/dao/UsersDao.java
+++ src/main/java/org/sda/twitter/database/dao/UsersDao.java
@@ -28,29 +28,30 @@ public class UsersDao {
         }
         return false;
     }
+
     public int hasUser(String login, String password) {
-        int id=-1;
+        int id = -1;
         try (Connection connection = datasourceConfiguration.getConnection();
              PreparedStatement statement = connection.prepareStatement("SELECT id FROM users WHERE login = ? AND password = ?")) {
             statement.setString(1, login);
             statement.setString(2, password);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next())
-                id=resultSet.getInt(1);
+                id = resultSet.getInt(1);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
         return id;
     }
 
-    public List<User> findAll(){
-        List<User> userList=new ArrayList<>();
+    public List<User> findAll() {
+        List<User> userList = new ArrayList<>();
         String sql = "select * from users";
-        try(Connection connection=datasourceConfiguration.getConnection();
-        Statement statement=connection.createStatement();){
+        try (Connection connection = datasourceConfiguration.getConnection();
+             Statement statement = connection.createStatement();) {
             ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()){
-                userList.add(new User(resultSet.getInt(1),resultSet.getString(2),resultSet.getString(3)));
+            while (resultSet.next()) {
+                userList.add(new User(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3)));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -58,4 +59,19 @@ public class UsersDao {
         return userList;
     }
 
+    public List<User> findAll(int userIdToIgnore) {
+        List<User> userList = new ArrayList<>();
+        String sql = "select * from users";
+        try (Connection connection = datasourceConfiguration.getConnection();
+             Statement statement = connection.createStatement();) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                if (userIdToIgnore != resultSet.getInt(1))
+                    userList.add(new User(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
+    }
 }
