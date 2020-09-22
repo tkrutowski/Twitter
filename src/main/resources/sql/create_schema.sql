061c7e4d75c61155125d207a0c5da35e8eb105b0
Tomasz Krutowski <53702327+tkrutowski@users.noreply.github.com>
2020-09-22 23:25:58 +0200

    Update create_schema.sql

--- src/main/resources/sql/create_schema.sql
+++ src/main/resources/sql/create_schema.sql
@@ -2,7 +2,8 @@ create schema twitter;
 
 create table users (id int auto_increment primary key, login varchar(20) not null, password varchar(30) not null);
 
-create table tweets (id int auto_increment primary key, messsage varchar(255) not null, author_id int not null,
+create table tweets (id int auto_increment primary key, message varchar(255) not null, author_id int not null,
+published TIMESTAMP,
 foreign key (author_id) references users(id));
 
 create table followers (follower_id int not null, followed_id int not null,
